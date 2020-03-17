1bb7637d6217c3416d985462ebca4f9f2cc5a2b5
plump_albert <plumpalbert@gmail.com>
2020-03-18 01:20:08 +0300

    IP function

--- des.c
+++ des.c
@@ -1,5 +1,11 @@
 #include "des.h"
 
+const unsigned char IP_TABLE[64] = {
+    58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+    57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
+
 unsigned char swap_bits(unsigned char x, char i, char j) {
   char left = (sizeof(unsigned char) * 8 - i);
   char right = (sizeof(unsigned char) * 8 - j);
@@ -11,6 +17,24 @@ unsigned char swap_bits(unsigned char x, char i, char j) {
   return x ^ xor;
 }
 
-uint64_t ip(uint64_t block) {
-return 0;
+long ip(long block) {
+  long result = 0;
+  for (int i = 0; i < sizeof(IP_TABLE); ++i) {
+    if (TestBit(block, 64, IP_TABLE[i])) {
+      SetBit(result, 64, i);
+      // printf("->\tIP[%d]: BLOCK[%d] -> %016lx\n", i, IP_TABLE[i], result);
+    } else
+      ClearBit(result, 64, i);
+  }
+  return result;
+}
+
+long cycle(long prev) {
+  printf("Prev = %016lx\n", prev);
+  long L = prev >> 32;
+  printf("L\t = %08lx\n", L);
+  int R = prev;
+  printf("R\t =         %08x\n", R);
+  // R = R ^ feistel(prev[1], key);
+  return (L << 32) | R;
 }
--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-#include "./des.h"
+#include "des.h"
 
 int main(int argc, char const *argv[]) {
-  unsigned char a = 1;
-  printf("a = %02X = %d\n", a, a);
-  for (int i = 1; i < 8; i++) {
-    unsigned char s = swap_bits(a, 8, 8 - i);
-    printf("swap = %02X = %d\n", s, s);
-  }
+  // unsigned char a = 1;
+  // printf("a = %02X = %d\n", a, a);
+  // for (int i = 1; i < 8; i++) {
+  //   unsigned char s = swap_bits(a, 8, 8 - i);
+  //   printf("swap = %02X = %d\n", s, s);
+  // }
+
+  long a = 1;
+  printf("a: %08lx\n", a);
+  long ip_res = ip(a);
+  printf("ip: %lu = %016lx\n", ip_res, ip_res);
+  printf("%d\n", cycle(1ul << 32));
   return 0;
 }
