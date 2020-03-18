dfa1d3074c045a50fc1ff4596468bef353c4d9f4
plump_albert <plumpalbert@gmail.com>
2020-03-18 22:14:54 +0300

    Some magic happened

--- des.c
+++ des.c
@@ -1,10 +1,17 @@
 #include "des.h"
+#include "colors.h"
 
-const unsigned char IP_TABLE[64] = {
+const unsigned char START_IP_TABLE[64] = {
     58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
     62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
     57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
     61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7};
+
+const unsigned char END_IP_TABLE[64] = {
+    40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9,  49, 17, 57, 25};
 
 unsigned char swap_bits(unsigned char x, char i, char j) {
   char left = (sizeof(unsigned char) * 8 - i);
@@ -17,19 +24,32 @@ unsigned char swap_bits(unsigned char x, char i, char j) {
   return x ^ xor;
 }
 
-long ip(long block) {
+unsigned long start_ip(unsigned long block) {
   long result = 0;
-  for (int i = 0; i < sizeof(IP_TABLE); ++i) {
-    if (TestBit(block, 64, IP_TABLE[i])) {
+  for (int i = 0; i < sizeof(START_IP_TABLE); ++i) {
+    if (TestBit(block, 64, START_IP_TABLE[i])) {
       SetBit(result, 64, i);
-      // printf("->\tIP[%d]: BLOCK[%d] -> %016lx\n", i, IP_TABLE[i], result);
+      // printf("->\tIP[%d]: BLOCK[%d] -> %016lx\n", i, START_IP_TABLE[i],
+      // result);
     } else
       ClearBit(result, 64, i);
   }
   return result;
 }
 
-long cycle(long prev) {
+unsigned long end_ip(unsigned long block) {
+  long result = 0;
+  for (int i = 0; i < sizeof(END_IP_TABLE); ++i) {
+    if (TestBit(block, 64, END_IP_TABLE[i])) {
+      SetBit(result, 64, i);
+      // printf("->\tIP[%d]: BLOCK[%d] -> %016lx\n", i, END_IP_TABLE[i], result);
+    } else
+      ClearBit(result, 64, i);
+  }
+  return result;
+}
+
+unsigned long cycle(unsigned long prev) {
   printf("Prev = %016lx\n", prev);
   long L = prev >> 32;
   printf("L\t = %08lx\n", L);
@@ -37,4 +57,30 @@ long cycle(long prev) {
   printf("R\t =         %08x\n", R);
   // R = R ^ feistel(prev[1], key);
   return (L << 32) | R;
+}
+
+unsigned char const* generate_key() {
+  FILE* pFile;
+  unsigned char* key = malloc(sizeof(unsigned char) * DES_KEY_LENGTH);
+
+  pFile = fopen("/dev/random", "r");
+  if (pFile == NULL) {
+    printf("Couldn't open /dev/random.\n\t => Check your privileges\n");
+    exit(2);
+  }
+  fgets(key, DES_KEY_LENGTH, pFile);
+  fclose(pFile);
+  return key;
+}
+
+unsigned long encrypt(unsigned long data) {
+  data = start_ip(data);
+  unsigned char const* key = generate_key();
+  
+  for (char i = 0; i < 16; ++i) {
+    printf("%s-> Cycle #%d%s\n", GREEN, i + 1, RESET);
+    data = cycle(data);
+    printf("\n"); 
+  }
+  return end_ip(data);
 }
--- des.h
+++ des.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
 
 /**
  * Sets k-th bit in A
@@ -8,7 +10,12 @@
 #define ClearBit(A, n, k) A &= ~(1ul << (n - k))
 #define TestBit(A, n, k) (A & (1ul << (n - k)))
 
+#define DES_KEY_LENGTH 7
+
 unsigned char swap_bits(unsigned char x, char i, char j);
 
-long ip(long);
-long cycle(long);
+unsigned long start_ip(unsigned long);
+unsigned long end_ip(unsigned long);
+unsigned long cycle(unsigned long);
+unsigned char const* generate_key();
+unsigned long encrypt(unsigned long);
